b40b042150b11f0d90fb2da398fa0c1fdc5ab9cb
Valeriano Sanjurjo <vsanjurjo7@gmail.com>
2020-08-29 14:57:37 -0300

    Initial commit

---
+++ src/main/java/com/octo/project/startproject/OctoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.octo.project.startproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OctoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OctoProjectApplication.class, args);
+	}
+
+}
